9953b0b2ada1f09748c1609822c4ad2ca3490897
Jakub Zygmunt <kubazygmunt99@gmail.com>
2021-11-08 22:21:46 +0100

    spring-boot project initialization

---
+++ server/src/test/java/com/changeTome/changeTome/ChangeTomeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.changeTome.changeTome;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChangeTomeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
